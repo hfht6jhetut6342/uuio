412327c2402d86c553d7daf2d246705954e0ad9c
hfht6jhetut6342 <120083505+hfht6jhetut6342@users.noreply.github.com>
2023-01-05 20:39:45 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 export UUID=4dc67b34-e3b1-4d86-b725-3af07f2a8d9d
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.www.wyliodrin.com
+export ProxySite=www.wyliodrin.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
